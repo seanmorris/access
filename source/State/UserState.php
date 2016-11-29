0a0243d5814fff1b4dd6f1be66fde2149d7f112d
Sean Morris <sean@arcade>
2016-11-29 08:39:41 -0500

    Package meta.

--- source/State/UserState.php
+++ source/State/UserState.php
@@ -4,7 +4,46 @@ class UserState extends \SeanMorris\PressKit\State
 {
 	protected static
 	$states	= [
-		0 => [
+		-1 => [
+			'create'	=> TRUE
+			, 'read'	 => TRUE
+			, 'update'	 => [TRUE, 'SeanMorris\Access\Role\Moderator']
+			, 'delete'	 => [FALSE, 'SeanMorris\Access\Role\Administrator']
+			, '$id'	=> [
+				'write'  => FALSE
+				, 'read' => TRUE
+			]
+			, '$created'	=> [
+				'write'  => [FALSE, 'SeanMorris\Access\Role\Administrator']
+				, 'read' => TRUE
+			]
+			, '$username'	=> [
+				'write'  => TRUE
+				, 'read' => TRUE
+			]
+			, '$email'	=> [
+				'write'  => TRUE
+				, 'read' => [TRUE, 'SeanMorris\Access\Role\Moderator']
+			]
+			, '$password'=> [
+				'write'  => TRUE
+				, 'read' => FALSE
+			]
+			, '$avatar'	=> [
+				'write'  => TRUE
+				, 'read' => TRUE
+			]
+			, '$roles'	=> [
+				'write'  => [FALSE, 'SeanMorris\Access\Role\Administrator']
+				, 'read' => TRUE
+			]
+			, '$state'	=> [
+				'write'  => [FALSE, 'SeanMorris\Access\Role\Administrator']
+				, 'read' => TRUE
+			]
+			, '$class'   => FALSE
+		]
+		, 0 => [
 			'create'	=> TRUE
 			, 'read'	 => TRUE
 			, 'update'	 => [TRUE, 'SeanMorris\Access\Role\Moderator']
@@ -32,10 +71,6 @@ class UserState extends \SeanMorris\PressKit\State
 			, '$avatar'	=> [
 				'write'  => [TRUE, 'SeanMorris\Access\Role\Moderator']
 				, 'read' => TRUE
-			]
-			, '$email'	=> [
-				'write'  => [FALSE, 'SeanMorris\Access\Role\Administrator']
-				, 'read' => [TRUE, 'SeanMorris\Access\Role\Moderator']
 			]
 			, '$roles'	=> [
 				'write'  => [FALSE, 'SeanMorris\Access\Role\Administrator']
